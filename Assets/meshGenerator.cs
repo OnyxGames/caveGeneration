e71a8fd7f90cb241434329b4783c9a5dd0bb394f
unknown <cmmg24@mst.edu>
2015-07-31 16:52:59 -0500

    'caveGeneration

---
+++ Assets/meshGenerator.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class meshGenerator : MonoBehaviour {
+
+    public SquareGrid squareGrid;
+
+    public void GenerateMesh(int[,] map, float squareSize) {
+        squareGrid = new SquareGrid(map, squareSize);
+    }
+
+    public class SquareGrid {
+        public Square[,] squares;
+
+        public SquareGrid(int[,] map, float squareSize) {
+            int nodeCountX = map.GetLength(0);
+            int nodeCountY = map.GetLength(1);
+            float mapWidth = nodeCountX * squareSize;
+            float mapHeight = nodeCountY * squareSize;
+
+            ControlNode[,] controlNodes = new ControlNode[nodeCountX, nodeCountY];
+
+            for (int x = 0; x < nodeCountX; x++) {
+                for (int y = 0; y < nodeCountY; y++) {
+                    
+                }
+            }
+        }
+    }
+
+
+    public class Square {
+        public ControlNode topLeft, topRight, bottomLeft, bottomRight;
+        public Node centreTop, centreRight, centreBottom, centreLeft;
+
+        public Square(ControlNode _topLeft, ControlNode _topRight, ControlNode _bottomRight, ControlNode _bottomLeft) {
+            topLeft = _topLeft;
+            topRight = _topRight;
+            bottomLeft = _bottomLeft;
+            bottomRight = _bottomRight;
+
+            centreTop = topLeft.right;
+            centreRight = bottomRight.above;
+            centreBottom = bottomLeft.right;
+            centreLeft = bottomLeft.above;
+        }
+    }
+
+    public class Node {
+        public Vector3 position;
+        public int vertexIndex = -1;
+
+        public Node(Vector3 _pos) {
+            position = _pos;
+        }
+    }
+
+    public class ControlNode : Node {
+        public bool active;
+        public Node above, right;
+
+        public ControlNode(Vector3 _pos, bool _active, float squareSize) : base(_pos) {
+            active = _active;
+            above = new Node(position + Vector3.forward * squareSize / 2f);
+            right = new Node(position + Vector3.right * squareSize / 2f);
+        }
+    }
+}
